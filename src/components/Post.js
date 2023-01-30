253814718274b3abb2072b0e590ddc9e11ea8fba
Carol-88 <gralleta@gmail.com>
2023-01-30 11:33:40 +0100

    Creada version 0.1 Instagrom

---
+++ src/components/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Comment from "./Comment";
+
+function Post() {
+	return (
+		<div>
+			<Avatar />
+			<Comment />
+		</div>
+	);
+}
+
+export default Post;
